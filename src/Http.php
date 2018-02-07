5b3d6aabe4c9ee9b33fae05c7fce4bc1281bc03a
Benji Garcia <azael.garcia@flighthub.com>
2018-02-07 15:10:09 -0500

    Added Get function to make HTTP Request

--- src/Http.php
+++ src/Http.php
@@ -1,6 +1,33 @@
 <?php
 namespace AzaelCodes\BookingComClient;
+
 class Http
 {
-    
+    /**
+     * @param $url
+     * @param array $headers
+     * @return array
+     */
+    public static function get($url, array $headers)
+    {
+        $curlOpts = [
+            CURLOPT_URL => $url,
+            CURLOPT_VERBOSE => false,
+            CURLOPT_HEADER => false,
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_RETURNTRANSFER => true
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, $curlOpts);
+        $response = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        $result = [
+            'code' => $httpCode,
+            'response' => json_decode($response, true)
+        ];
+        return $result;
+    }
 }
