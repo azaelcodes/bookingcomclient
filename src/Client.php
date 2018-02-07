3b58c6076ffe7b88e8a6c1c2781ef962b7341fb1
Benji Garcia <azael.garcia@flighthub.com>
2018-02-07 15:00:08 -0500

    Added Client class and Http with nothing int it

---
+++ src/Client.php
@@ -0,0 +1,61 @@
+<?php
+class Client
+{
+    private $username;
+    private $password;
+    private $apiUrl;
+
+    const API_URL       = 'https://distribution-xml.booking.com/';
+    const API_VERSION   = '2.0';
+    const JSON_CONTENT  = 'json';
+
+    public function __construct()
+    {
+        $this->buildAuthCredentials();
+        $this->buildBookingApiUrl();
+    }
+
+    /**
+     * Set the API credentials, obtained from the Config files.
+     */
+    public function buildAuthCredentials()
+    {
+        // Read from a config file
+        $credentials = [];
+        $this->username = $credentials['username'];
+        $this->password = $credentials['password'];
+    }
+
+    /**
+     * Build the Booking.com Api URL
+     */
+    public function buildBookingApiUrl()
+    {
+        $this->apiUrl = static::API_URL . static::API_VERSION . '/' . static::JSON_CONTENT;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBookingApiUrl()
+    {
+        return $this->apiUrl;
+    }
+
+    /**
+     * @param array $requestedInfo
+     * @return array
+     */
+    public function getHotelData(array $requestedInfo)
+    {
+        $headers = [
+            'Content-Type:application/json',
+            'Authorization: Basic ' . base64_encode($this->username . ':' . $this->password),
+            'cache-control: no-cache'
+        ];
+        $url = $this->getBookingApiUrl() . '/hotels?' . http_build_query($requestedInfo);
+        $request = Http::get($url, $headers);
+        $data = $request['code'] == 200 ? $request['response']['result'] : null;
+        return $data;
+    }
+}
